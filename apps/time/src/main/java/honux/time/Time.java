ba5575fe018100b5685bcfc407ddf9487d98be1d
Hoyoung Jung <honux77@gmail.com>
2025-06-18 17:34:07 +0900

    도커 파일로 빌드

--- apps/time/src/main/java/honux/time/Time.java
+++ apps/time/src/main/java/honux/time/Time.java
@@ -2,9 +2,11 @@ package honux.time;
 
 import java.time.LocalDateTime;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class Time {
     @GetMapping("/time")
     public String getTime() {
